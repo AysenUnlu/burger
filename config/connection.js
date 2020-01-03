d73eff70e0ad67cec1676310df73712a33225bc6
Aysen Unlu <aysenunlu@gmail.com>
2020-01-02 23:35:22 -0800

    heroku db credentials

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 //Set up MySQL connection
 const mysql=require("mysql");
 
-const connection=mysql.createConnection({
+/*const connection=mysql.createConnection({
     host:"localhost",
     port:3306,
     user:"root",
     password:"",
     database:"burgers_db"
+});*/
+//heroku deployment
+const connection=mysql.createConnection({
+    host:'us-cdbr-iron-east-05.cleardb.net',
+    port:3306,
+    user:'b27745781fcb1e',
+    password:"d969ce9c",
+    database:"heroku_6ade62fa4727325"
 });
 
 //Make connection
